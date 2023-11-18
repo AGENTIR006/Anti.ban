250c5d24ef704e347fd83fa182ff93e96d80666e
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-11-18 17:06:10 +0300

    Create ANTI_BAN.lua

---
+++ ANTI_BAN.lua
@@ -0,0 +1,3 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+local w = library:CreateWindow("ANTI BAN") 
+local b = w:CreateFolder("ANTI BAN")
